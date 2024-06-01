392d8794cbe158c8bf0ac8666639e039f9233237
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-01 20:39:24 +0700

    fix: eslint errors

--- src/utilities/CalculateCentroid.ts
+++ src/utilities/CalculateCentroid.ts
@@ -1,13 +1,13 @@
-const calculateMiddlePoint = (geoJsonData) => {
+const calculateMiddlePoint = (geoJsonData:any) => {
   // Extract coordinates
-  const coordinates = geoJsonData.features.map(feature => feature.geometry.coordinates);
+  const coordinates = geoJsonData.features.map((feature:any) => feature.geometry.coordinates);
 
   // Calculate the average of the latitudes and longitudes (swapping their positions)
-  const middlePoint = coordinates.reduce((acc, coord) => {
+  const middlePoint = coordinates.reduce((acc:any, coord:any) => {
     acc[0] += coord[1]; // latitude
     acc[1] += coord[0]; // longitude
     return acc;
-  }, [0, 0]).map(coord => coord / coordinates.length);
+  }, [0, 0]).map((coord:any) => coord / coordinates.length);
 
   return middlePoint;
 };
